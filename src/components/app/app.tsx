93ba3d144ae73c5072e0751d7ba3785c7f7eab46
natalia <skaweb@yandex.ru>
2026-09-08 04:30:48 +0300

    Добавить маршруты

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
-import { ConstructorPage } from '@pages';
-import { Routes, Route } from 'react-router-dom';
+import { ConstructorPage, Feed, ForgotPassword, Login, NotFound404, Profile, ProfileOrders, Register, ResetPassword } from '@pages';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 import { Preloader } from '@ui';
+import { ProtectedRoute } from '../protected-route';
 
 const App = () => {
   /** TODO: взять переменные из стора */
@@ -12,10 +13,84 @@ const App = () => {
   const ingredients = [];
   const error = null;
 
+  const location = useLocation();
+  const navigate = useNavigate();
+  const backgroundLocation = location.state?.backgroundLocation;
+
+  const handleCloseModal = () => {navigate(-1)};
+
   return (
-    <Routes>
-      
-    </Routes>
+    <>
+      <Routes location={backgroundLocation ?? location}>
+        <Route path="/" element={<ConstructorPage />} />
+        <Route path="/feed" element={<Feed />} />
+
+        <Route path="/login" element={
+            <ProtectedRoute>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/register" element={
+            <ProtectedRoute>
+              <Register />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/forgot-password" element={
+            <ProtectedRoute>
+              <ForgotPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="reset-password" element={
+          <ProtectedRoute>
+            <ResetPassword />
+          </ProtectedRoute>
+          }
+        />
+        <Route path="/profile" element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+          }
+        />
+        <Route path="/profile/orders" element={
+          <ProtectedRoute>
+            <ProfileOrders />
+          </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound404 />} />
+      </Routes>
+
+      {
+        backgroundLocation && 
+        <Routes>
+          <Route path="/feed/:number" element={
+            <Modal title="" onClose={handleCloseModal}>
+              <OrderInfo />
+            </Modal>
+            }
+          />
+          <Route path="/ingredients/:id" element={
+            <Modal title="" onClose={handleCloseModal}>
+              <IngredientDetails />
+            </Modal>
+            }
+          />
+          <Route path="/profile/orders/:number" element={
+            <ProtectedRoute>
+              <Modal title="" onClose={handleCloseModal}>
+                <OrderInfo />
+              </Modal>
+            </ProtectedRoute>
+            }
+          />
+        </Routes>
+      }
+    </>
+
     // <div className={styles.app}>
     //   <AppHeader />
     //   {isIngredientsLoading ? (
