02291039ccdbd78362751f120cc6462cc5e1e0d1
natalia <skaweb@yandex.ru>
2026-09-17 21:00:46 +0300

    Добавить страницу регистрации

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,18 +1,36 @@
-import { FC, SyntheticEvent, useState } from 'react';
+import { FC, SyntheticEvent, useState, useEffect } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import { useNavigate } from 'react-router-dom';
+import {
+  selectUserError,
+  selectIsAuth
+} from '../../services/selectors/userSelectors';
+import { register } from '../../services/slices/userSlice';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const errorText = useSelector(selectUserError);
+  const isAuth = useSelector(selectIsAuth);
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate('/');
+    }
+  }, [isAuth, navigate]);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(register({ email, name: userName, password }));
   };
 
   return (
     <RegisterUI
-      errorText=''
+      errorText={errorText ?? ''}
       email={email}
       userName={userName}
       password={password}
