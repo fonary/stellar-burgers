7f34e13f3ba776632050b6e454f944aefe2b434b
natalia <skaweb@yandex.ru>
2026-09-18 20:35:44 +0300

    Выход из профиля

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,11 +1,25 @@
-import { FC } from 'react';
-import { useLocation } from 'react-router-dom';
+import { FC, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { ProfileMenuUI } from '@ui';
+import { useDispatch, useSelector } from '../../services/store';
+import { logout } from '@slices';
+import { selectIsAuth } from '@selectors';
 
 export const ProfileMenu: FC = () => {
   const { pathname } = useLocation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isAuth = useSelector(selectIsAuth);
 
-  const handleLogout = () => {};
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  useEffect(() => {
+    if (!isAuth) {
+      navigate('/');
+    }
+  }, [isAuth, navigate]);
 
   return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };
