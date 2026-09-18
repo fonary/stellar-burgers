58061d36d3a71343e32bc5eaf8463410e966e9ca
natalia <skaweb@yandex.ru>
2026-09-18 20:45:26 +0300

    Сделать активным хедер

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,25 +1,17 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { ProfileMenuUI } from '@ui';
-import { useDispatch, useSelector } from '../../services/store';
+import { useDispatch } from '../../services/store';
 import { logout } from '@slices';
-import { selectIsAuth } from '@selectors';
 
 export const ProfileMenu: FC = () => {
   const { pathname } = useLocation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const isAuth = useSelector(selectIsAuth);
 
   const handleLogout = () => {
-    dispatch(logout());
+    dispatch(logout()).then(() => navigate('/'));
   };
-
-  useEffect(() => {
-    if (!isAuth) {
-      navigate('/');
-    }
-  }, [isAuth, navigate]);
 
   return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };
